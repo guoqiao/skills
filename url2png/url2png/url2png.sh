2b0dde89ba975daf5e6d56c9c45ea1c3dc2f84ae
guoqiao <guoqiao@gmail.com>
2026-02-12 15:33:48 +1300

    url2png.sh: update

--- url2png/url2png/url2png.sh
+++ url2png/url2png/url2png.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
+set -ue -o pipefail
+
 url="${1:-https://simonwillison.net/2026/Feb/10/showboat-and-rodney}"
-outdir=${2:-~/Pictures}
+
+outdir=$(mktemp -d)
 mkdir -p ${outdir}
 # cd into outdir, let shot-scraper save with proper name
 cd ${outdir}
 
+unset UV_ENV_FILE
+uvx --no-env-file shot-scraper install --browser chromium || true
 # take long screenshot for iPhone by default
-uvx shot-scraper shot "${url}" \
+uvx --no-env-file shot-scraper shot "${url}" \
     --browser chromium \
     --width 390 \
     --retina \
-    --wait 10000 \
-    --timeout 30000
+    --wait 5000 \
+    --timeout 30000 \
+    --silent
+
+find ${outdir} -maxdepth 1 -type f -name "*.png"
